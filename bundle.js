404184969dc2026c4efb38f54830f91932c6a7b0
Abun Yamin <abunyamin2@gmail.com>
2023-08-06 12:24:19 +0700

    Updates

--- bundle.js
+++ bundle.js
@@ -1,1 +1,141 @@
-(()=>{"use strict";var n={};n.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(n){if("object"==typeof window)return window}}(),(()=>{var e;n.g.importScripts&&(e=n.g.location+"");var s=n.g.document;if(!e&&s&&(s.currentScript&&(e=s.currentScript.src),!e)){var t=s.getElementsByTagName("script");if(t.length)for(var a=t.length-1;a>-1&&!e;)e=t[a--].src}if(!e)throw new Error("Automatic publicPath is not supported in this browser");e=e.replace(/#.*$/,"").replace(/\?.*$/,"").replace(/\/[^\/]+$/,"/"),n.p=e})(),n.p;const e=screen.availWidth,s=screen.availHeight,t=window.devicePixelRatio,a=document.querySelector(".viewport__size"),i=document.querySelector(".screen__size"),p=document.querySelector(".dpr__size");i.innerHTML=`<span class="size"><span>${e}</span><span class="px">px</span></span><span class="size__x">x</span><span class="size"><span>${s}</span><span class="px">px</span></span>`,p.innerHTML=`<span>DPR : ${parseFloat(t.toFixed(2))}</span>`;const c=()=>{const n=window.innerWidth,e=window.innerHeight;a.innerHTML=`<span class="size"><span>${n}</span><span class="px">px</span></span><span class="size__x">x</span><span class="size"><span>${e}</span><span class="px">px</span></span>`};window.addEventListener("resize",(()=>c())),c();const r=document.getElementById("modal"),o=document.getElementById("modal__btn"),l=document.getElementById("modal__close");o.onclick=function(){r.style.display="flex"},l.onclick=function(){r.style.display="none"}})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/github.png":
+/*!*******************************!*\
+  !*** ./src/assets/github.png ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"assets/github.png\");\n\n//# sourceURL=webpack://javascript/./src/assets/github.png?");
+
+/***/ }),
+
+/***/ "./src/styles.css":
+/*!************************!*\
+  !*** ./src/styles.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://javascript/./src/styles.css?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n/* harmony import */ var _assets_github_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./assets/github.png */ \"./src/assets/github.png\");\n\r\n\r\n\r\nconst screenWidth = screen.availWidth;\r\nconst screenHeight = screen.availHeight;\r\nconst dpr = window.devicePixelRatio;\r\n\r\nconst viewportSize = document.querySelector('.viewport__size');\r\nconst screenSize = document.querySelector('.screen__size');\r\nconst dprSize = document.querySelector('.dpr__size');\r\n\r\nscreenSize.innerHTML = `<span class=\"size\"><span>${screenWidth}</span><span class=\"px\">px</span></span><span class=\"size__x\">x</span><span class=\"size\"><span>${screenHeight}</span><span class=\"px\">px</span></span>`;\r\ndprSize.innerHTML = `<span>DPR : ${parseFloat(dpr.toFixed(2))}</span>`;\r\n\r\nconst updateViewport = () => {\r\n  const viewportWidth = window.innerWidth;\r\n  const viewportHeight = window.innerHeight;\r\n  viewportSize.innerHTML = `<span class=\"size\"><span>${viewportWidth}</span><span class=\"px\">px</span></span><span class=\"size__x\">x</span><span class=\"size\"><span>${viewportHeight}</span><span class=\"px\">px</span></span>`;\r\n};\r\n\r\nwindow.addEventListener('resize', () => updateViewport());\r\n\r\nupdateViewport();\r\n\r\n// modal\r\n\r\nconst modal = document.getElementById('modal');\r\n\r\nconst modalBtn = document.getElementById('modal__btn');\r\nconst modalBtnClose = document.getElementById('modal__close');\r\n\r\nmodalBtn.onclick = function() {\r\n  modal.style.display = \"flex\"\r\n}\r\n\r\nmodalBtnClose.onclick = function() {\r\n  modal.style.display = \"none\"\r\n}\r\n\n\n//# sourceURL=webpack://javascript/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
